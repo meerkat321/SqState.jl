8361e5d2f0a4fd89ffab831ddab773e78009cc92
JingYu Ning <foldfelis@gmail.com>
2021-06-14 01:59:25 +0800

    refactor

--- test/training_data.jl
+++ test/training_data.jl
@@ -3,10 +3,6 @@ using DataDeps
 using HDF5
 using Plots
 
-function gen_training_data()
-
-end
-
 function main()
     path = mkpath(joinpath(datadep"SqState", "training_data"))
     file_name = joinpath(path, "training_data.h5")
@@ -16,19 +12,20 @@ function main()
     θ = π/4
     n̄ = 0.3
 
-    sp_lock = Threads.SpinLock()
+    # sp_lock = Threads.SpinLock()
     # for r in LinRange(0., 16., 35)
     #     for θ in LinRange(0., 2π, 60)
     #         for n̄ in LinRange(0., 0.5, 50)
                 data_name = "$(round(r, digits=5)),$(round(θ, digits=5)),$(round(n̄, digits=5))"
                 data = gen_data(CoherentState(α(r, θ), rep=StateMatrix), n=4096)
 
-                lock(sp_lock) do
-                    h5write(file_name, "$data_name", data)
-                end
+                # lock(sp_lock) do
+                #     h5write(file_name, "$data_name", data)
+                # end
     #         end
     #     end
     # end
 
-    scatter(data[:, 1], data[:, 2])
+    p = scatter(data[:, 1], data[:, 2])
+    savefig(p, "alpha.png")
 end
