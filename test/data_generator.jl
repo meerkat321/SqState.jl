63238dd550a7955db36badf5ff7ad833d2ea20de
JingYu Ning <foldfelis@gmail.com>
2021-07-03 00:08:09 +0800

    add test for single point pdf

--- test/data_generator.jl
+++ test/data_generator.jl
@@ -1,9 +1,14 @@
 using KernelDensity
 
-@testset "Gaussian state data generator" begin
+@testset "pdf and Gaussian state data generator" begin
     state = SqueezedThermalState(ξ(1., π/4), 0.5)
     θs = LinRange(0, 2π, 10)
     xs = LinRange(-10, 10, 10)
+
+    ground_truth_pdf = SqState.pdf(state, θs, xs)
+
+    single_point_pdf = (θ, x) -> SqState.pdf(state, θ, x)
+    @test single_point_pdf.(θs, xs') ≈ ground_truth_pdf
 
     n = 100000
     data = gen_gaussian_training_data(state, n)
@@ -11,8 +16,6 @@ using KernelDensity
         kde((LinRange(0, 2π, n), data)),
         θs, xs
     )
-
-    ground_truth_pdf = SqState.pdf(state, θs, xs)
 
     @test sum(sampled_pdf - ground_truth_pdf) / n < 1e-4
 end
