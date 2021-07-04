14d95b4c899dc019ebe2b76ded8f92c326ff4fd2
JingYu Ning <foldfelis@gmail.com>
2021-07-04 23:01:34 +0800

    enable test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ ENV["GKSwstype"]="nul"
 
 @testset "SqState.jl" begin
 
-    # include("state/state.jl")
+    include("state/state.jl")
     include("data_generator.jl")
 
-    # include("read.jl")
-    # include("polynomial.jl")
-    # include("wigner.jl")
-    # include("plot.jl")
+    include("read.jl")
+    include("polynomial.jl")
+    include("wigner.jl")
+    include("plot.jl")
 
 end
