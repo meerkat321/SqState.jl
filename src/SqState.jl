699eaeec8c064d45799b5138d7535191d6f46b5c
JingYu Ning <foldfelis@gmail.com>
2021-07-07 13:48:13 +0800

    tear down

--- src/SqState.jl
+++ src/SqState.jl
@@ -1,25 +1,11 @@
 module SqState
     using DataDeps
-
-    const DIM = 70
+    using QuantumStateBase
 
     function __init__()
-        register(DataDep(
-            "SqState",
-            """
-            Data for SqState.
-            """,
-            ""
-        ))
-
+        register(DataDep("SqState", """Data for SqState.""", ""))
         mkpath(joinpath(DataDeps.standard_loadpath[1], "SqState"))
     end
 
-    include("state/state.jl")
-    include("data_generator.jl")
-
-    include("read.jl")
-    include("polynomial.jl")
-    include("wigner.jl")
-    include("plot.jl")
+    # include("plot.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,5 @@ using Test
 ENV["GKSwstype"]="nul"
 
 @testset "SqState.jl" begin
-
-    include("state/state.jl")
-    include("data_generator.jl")
-
-    include("read.jl")
-    include("polynomial.jl")
-    include("wigner.jl")
-    include("plot.jl")
-
+    # include("plot.jl")
 end
