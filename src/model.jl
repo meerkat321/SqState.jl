a97d065c737acce5e7c3e54025c1c2ba2310c9ae
JingYu Ning <foldfelis@gmail.com>
2021-07-07 20:07:08 +0800

    residual_block

--- src/model.jl
+++ src/model.jl
@@ -1,3 +1,4 @@
+using Flux: stride
 using SqState
 using LinearAlgebra
 using Flux
@@ -20,15 +21,40 @@ function loss(ŷ, 𝐲)
     return Flux.mse(l * l', 𝐲)
 end
 
+function conv_layers()
+    return Chain(
+        Conv((4, ), 1=>128, relu, stride=1, pad=SamePad()),
+        Conv((1, ), 128=>64, relu, stride=1, pad=SamePad()),
+        Conv((4, ), 64=>64, relu, stride=1, pad=SamePad()),
+        Conv((1, ), 64=>128, relu, stride=1, pad=SamePad())
+    )
+end
+
+function residual_block()
+    return Chain(
+        x -> conv_layers()(x) + Conv((3, ), 1=>128, relu, stride=1, pad=1)(x),
+        MeanPool((2, ))
+    )
+end
+
+function model()
+    return Chain(
+        residual_block(),
+    )
+end
+
 file_names = readdir(SqState.training_data_path())
 f = jldopen(joinpath(SqState.training_data_path(), file_names[1]), "r")
 points = f["points"]
 𝛒s = f["𝛒s"]
 
-for i in 1:1
+for i in 1:1 # 10000
     x = Float32.(points[:, i])
     y = ComplexF32.(𝛒s[i])
 
-    y_dummy = rand(ComplexF32, sum(1:70))
-    println(loss(y_dummy, y))
+    x = reshape(x, (4096, 1, 1)) # 4096 points 1 channel, 1 data in a batch
+    ŷ = model()(x)
+    println(size(ŷ))
+
+    # println(loss(y, y))
 end
