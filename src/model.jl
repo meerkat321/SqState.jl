19bd3d1b403ce7f6a9b21345f637e3479e9b5470
JingYu Ning <foldfelis@gmail.com>
2021-07-07 23:56:22 +0800

    fix gradient  model bug

--- src/model.jl
+++ src/model.jl
@@ -11,54 +11,53 @@ end
 
 dim = 70
 
-function loss(l̂, 𝐲)
-    𝐥̂_real = reshape(l̂[1:(dim*dim)], (dim, dim))
-    𝐥̂_imag = reshape(l̂[(dim*dim+1):end], (dim, dim))
-
-    𝐥̂ = 𝐥̂_real + im * 𝐥̂_imag
-
-    # l ∈ (dim, n)
-    # l * l' ∈ (dim, dim) # positive semi-definite matrix
-    # Flux.mse(l * l', 𝐲)
-
-    return Flux.mse(𝐥̂ * 𝐥̂', 𝐲)
-end
-
 function conv_layers(ch::NTuple{4, <:Integer}, kernel_size::NTuple{3, <:Integer})
-    return Chain(
+    return [
         Conv((kernel_size[1], ), ch[1]=>ch[2], pad=SamePad()),
         BatchNorm(ch[2], relu),
         Conv((kernel_size[2], ), ch[2]=>ch[3], pad=SamePad()),
         BatchNorm(ch[3], relu),
         Conv((kernel_size[3], ), ch[3]=>ch[4], pad=SamePad()),
         BatchNorm(ch[4], relu),
-    )
+    ]
 end
 
 function residual_block()
-    return Chain(
-        x -> conv_layers((128, 64, 64, 128), (1, 4, 1))(x) + x,
+    convs = conv_layers((128, 64, 64, 128), (1, 4, 1))
+
+    return [
+        x -> Chain(convs...)(x) + x,
         MeanPool((2, ))
-    )
+    ]
 end
 
 function model()
+    res_blk = vcat([residual_block() for _ = 1:10]...)
+
     return Chain(
         Conv((4, ), 1=>128, relu, pad=SamePad()),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
-        residual_block(),
+        Chain(res_blk...),
         flatten,
         Dense(4*128, 2048),
         Dense(2048, 2*dim*dim)
     )
+end
+
+m = model()
+
+function loss(x, 𝐲)
+    l̂ = m(x)
+    𝐥̂_real = reshape(l̂[1:(dim*dim)], (dim, dim))
+    𝐥̂_imag = reshape(l̂[(dim*dim+1):end], (dim, dim))
+
+    𝐥̂ = 𝐥̂_real + im * 𝐥̂_imag
+    𝛒̂ = 𝐥̂ * 𝐥̂'
+
+    # l ∈ (dim, n)
+    # l * l' ∈ (dim, dim) # positive semi-definite matrix
+    # Flux.mse(l * l', 𝐲)
+
+    return Flux.mse(real.(𝛒̂), real.(𝐲)) + Flux.mse(imag.(𝛒̂), imag.(𝐲))
 end
 
 file_names = readdir(SqState.training_data_path())
@@ -67,12 +66,11 @@ points = f["points"]
 𝛒s = f["𝛒s"]
 
 for i in 1:1 # 10000
-    x = Float32.(points[:, i])
+    x = reshape(Float32.(points[:, i]), (4096, 1, 1)) # 4096 points 1 channel, 1 data in a batch
     y = ComplexF32.(𝛒s[i])
 
-    x = reshape(x, (4096, 1, 1)) # 4096 points 1 channel, 1 data in a batch
-    ŷ = reshape(model()(x), :)
-
-    @show size(ŷ)
-    @show loss(ŷ, y)
+    @show size(reshape(m(x), :))
+    @show loss(x, y)
+    @show gradient(x->sum(m(x)), x)
+    # @show gradient(x->loss(x, y), x)
 end
