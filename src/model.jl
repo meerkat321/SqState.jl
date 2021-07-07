f7b4e23b8ec239846eafd8938a5a86a7fb7d6d1b
JingYu Ning <foldfelis@gmail.com>
2021-07-08 01:28:57 +0800

    COMPLEX neural network

--- src/model.jl
+++ src/model.jl
@@ -11,14 +11,38 @@ end
 
 dim = 70
 
+vanilla_softplus(x) = log1p(exp(x))
+
+c_glorot_uniform(dims...) = Flux.glorot_uniform(dims...) + Flux.glorot_uniform(dims...) * im
+
+function C_BatchNorm(chs::Int, λ=identity;
+    initβ = i -> zeros(ComplexF32, i),
+    initγ = i -> ones(ComplexF32, i),
+    affine=true, track_stats=true,
+    ϵ=1f-5 + 1f-5im, momentum=1f-1 + 1f-1im
+)
+
+    β = affine ? initβ(chs) : nothing
+    γ = affine ? initγ(chs) : nothing
+    μ = track_stats ? zeros(ComplexF32, chs) : nothing
+    σ² = track_stats ? ones(ComplexF32, chs) : nothing
+
+    return Flux.BatchNorm(
+        λ, β, γ,
+        μ, σ², ϵ, momentum,
+        affine, track_stats,
+        nothing, chs
+    )
+end
+
 function conv_layers(ch::NTuple{4, <:Integer}, kernel_size::NTuple{3, <:Integer})
     return [
-        Conv((kernel_size[1], ), ch[1]=>ch[2], pad=SamePad()),
-        BatchNorm(ch[2], relu),
-        Conv((kernel_size[2], ), ch[2]=>ch[3], pad=SamePad()),
-        BatchNorm(ch[3], relu),
-        Conv((kernel_size[3], ), ch[3]=>ch[4], pad=SamePad()),
-        BatchNorm(ch[4], relu),
+        Conv((kernel_size[1], ), ch[1]=>ch[2], pad=SamePad(), init=c_glorot_uniform),
+        C_BatchNorm(ch[2], vanilla_softplus),
+        Conv((kernel_size[2], ), ch[2]=>ch[3], pad=SamePad(), init=c_glorot_uniform),
+        C_BatchNorm(ch[3], vanilla_softplus),
+        Conv((kernel_size[3], ), ch[3]=>ch[4], pad=SamePad(), init=c_glorot_uniform),
+        C_BatchNorm(ch[4], vanilla_softplus),
     ]
 end
 
@@ -35,47 +59,21 @@ function model()
     res_blk = vcat([residual_block() for _ = 1:10]...)
 
     return Chain(
-        Conv((4, ), 1=>128, relu, pad=SamePad()),
+        Conv((4, ), 1=>128, vanilla_softplus, pad=SamePad()),
         Chain(res_blk...),
         flatten,
         Dense(4*128, 2048),
-        Dense(2048, 2*dim*dim)
+        Dense(2048, dim*dim)
     )
 end
 
 m = model()
 
 function loss(x, 𝐲)
-    l̂ = m(x)
-    𝐥̂_real = reshape(l̂[1:(dim*dim)], (dim, dim))
-    𝐥̂_imag = reshape(l̂[(dim*dim+1):end], (dim, dim))
-
-    𝐥̂ = 𝐥̂_real + im * 𝐥̂_imag
+    𝐥̂ = reshape(m(x), (dim, dim))
     𝛒̂ = 𝐥̂ * 𝐥̂'
-    𝛒̂ = hcat(real.(𝛒̂), imag.(𝛒̂))
 
-    # l ∈ (dim, n)
-    # l * l' ∈ (dim, dim) # positive semi-definite matrix
-    # Flux.mse(l * l', 𝐲)
-
-    return Flux.mse(𝛒̂, 𝐲)
-end
-
-# DEBUG: remove complex relative operation
-function loss_y(l̂, 𝐲)
-    # l̂ = m(x)
-    𝐥̂_real = reshape(l̂[1:(dim*dim)], (dim, dim))
-    𝐥̂_imag = reshape(l̂[(dim*dim+1):end], (dim, dim))
-
-    𝐥̂ = 𝐥̂_real + im * 𝐥̂_imag
-    𝛒̂ = 𝐥̂ * 𝐥̂'
-    𝛒̂ = hcat(real.(𝛒̂), imag.(𝛒̂))
-
-    # l ∈ (dim, n)
-    # l * l' ∈ (dim, dim) # positive semi-definite matrix
-    # Flux.mse(l * l', 𝐲)
-
-    return Flux.mse(𝛒̂, 𝐲)
+    return abs(Flux.mse(𝛒̂, 𝐲))
 end
 
 file_names = readdir(SqState.training_data_path())
@@ -84,13 +82,11 @@ points = f["points"]
 𝛒s = f["𝛒s"]
 
 for i in 1:1 # 10000
-    x = reshape(Float32.(points[:, i]), (4096, 1, 1)) # 4096 points 1 channel, 1 data in a batch
+    x = reshape(ComplexF32.(points[:, i]), (4096, 1, 1)) # 4096 points 1 channel, 1 data in a batch
     𝐲 = ComplexF32.(𝛒s[i])
-    𝐲 = hcat(real.(𝐲), imag.(𝐲))
 
     @show size(reshape(m(x), :))
     @show loss(x, 𝐲)
-    # @show gradient(x->sum(m(x)), x)
-    # @show gradient(x->loss(x, 𝐲), x)
-    @show gradient(l̂->loss_y(l̂, 𝐲), rand(Float32, 2*dim*dim))
+    @show gradient(x->sum(abs, m(x)), x)
+    @show gradient(x->loss(x, 𝐲), x)
 end
