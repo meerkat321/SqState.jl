fa747404898a840e51161a6f84e64bd9d97153c7
JingYu Ning <foldfelis@gmail.com>
2021-07-10 19:54:03 +0800

    refactor

--- src/model.jl
+++ src/model.jl
@@ -1,18 +1,7 @@
-using Flux: length
-using CUDA: length
 using SqState
 using Flux
 using Flux.Data: DataLoader
 using CUDA
-
-is_gpu = true
-
-if CUDA.has_cuda() && is_gpu
-    @info "CUDA is on"
-    CUDA.allowscalar(false)
-end
-
-dim = 70
 
 function conv_layers(ch::NTuple{4, <:Integer}, kernel_size::NTuple{3, <:Integer}, pad::NTuple{3, <:Any})
     return Chain(
@@ -25,14 +14,14 @@ function conv_layers(ch::NTuple{4, <:Integer}, kernel_size::NTuple{3, <:Integer}
     )
 end
 
-function conv_short_cut(ch::NTuple{2, <:Integer}, kernel_size::Integer, pad::Any)
+function short_cut(ch::NTuple{2, <:Integer}, kernel_size::Integer, pad::Any)
     return Chain(
         Conv((kernel_size, ), ch[1]=>ch[2], pad=pad),
         BatchNorm(ch[2])
     )
 end
 
-function model()
+function model(; dim=70)
     return Chain(
         # stage 0
         BatchNorm(1),
@@ -41,7 +30,7 @@ function model()
         # res 1
         Parallel(+,
             conv_layers((64, 32, 32, 96), (1, 15, 7), (0, 7, 3)),
-            conv_short_cut((64, 96), 1, 0),
+            short_cut((64, 96), 1, 0),
         ),
         x -> relu.(x),
         MeanPool((2, )),
@@ -57,7 +46,7 @@ function model()
         # res 4
         Parallel(+,
             conv_layers((96, 64, 64, 128), (1, 3, 1), (0, 1, 0)),
-            conv_short_cut((96, 128), 1, 0),
+            short_cut((96, 128), 1, 0),
         ),
         x -> relu.(x),
         # res 5
@@ -71,7 +60,7 @@ function model()
         # res 7
         Parallel(+,
             conv_layers((128, 96, 96, 196), (1, 3, 1), (0, 1, 0)),
-            conv_short_cut((128, 196), 1, 0),
+            short_cut((128, 196), 1, 0),
         ),
         x -> relu.(x),
         # res 8
@@ -96,53 +85,68 @@ function model()
     )
 end
 
-m = is_gpu ? model() |> gpu : model()
-ps = Flux.params(m)
-opt = ADAM(1e-2, (0.7, 0.9))
+function training_process(;
+    file_names=readdir(SqState.training_data_path()),
+    batch_size=100, epochs=10,
+    is_gpu=true
+)
+    if CUDA.has_cuda() && is_gpu
+        @info "CUDA is on"
+        CUDA.allowscalar(false)
+    end
 
-loss(x, y) = Flux.mse(m(x), y)
+    # prepare model
+    m = is_gpu ? model() |> gpu : model()
+    loss(x, y) = Flux.mse(m(x), y)
+    ps = Flux.params(m)
+    opt = ADAM(1e-2, (0.7, 0.9))
 
-file_names = readdir(SqState.training_data_path())
+    # jit model
+    @time begin
+        @info "jit..."
+        x, y = first(preprocess(file_names[end], batch_size=1))
+        x = is_gpu ? x |> gpu : x
+        y = is_gpu ? y |> gpu : y
+        gs = Flux.gradient(() -> loss(x, y), ps)
+        Flux.update!(opt, ps, gs)
+    end
 
-function warm_up_jit()
-    @info "jit..."
-    x, y = first(preprocess(file_names[end], batch_size=1))
-    x = is_gpu ? x |> gpu : x
-    y = is_gpu ? y |> gpu : y
-    gs = Flux.gradient(() -> loss(x, y), ps)
-    Flux.update!(opt, ps, gs)
-end
+    # prepare data
+    @info "numbers of data fragments: $(length(file_names)-1)"
+    data_loaders = Channel(5, spawn=true) do ch
+        for e in 1:epochs
+            for (i, file_name) in enumerate(file_names[1:(end-1)])
+                put!(ch, preprocess(file_name, batch_size=batch_size))
+                @info "Load epoch $(e), $(i)th files into buffer"
+            end
+        end
+    end
 
-@time warm_up_jit()
-
-@info "load data"
-data_fragments = Vector{DataLoader}(undef, length(file_names))
-@time @sync for (f, file_name) in enumerate(file_names)
-    Threads.@spawn data_fragments[f] = preprocess(file_name, batch_size=100)
-end
-
-for e in 1:1
-    @info "epoch: $e"
-    for (f, loader) in enumerate(data_fragments[1:(end-1)])
+    # training
+    for (t, loader) in enumerate(data_loaders)
         l = 0f0
         @time for (b, (x, y)) in enumerate(loader)
             x = is_gpu ? x |> gpu : x
             y = is_gpu ? y |> gpu : y
 
+            l += loss(x, y)
+
             gs = Flux.gradient(() -> loss(x, y), ps)
             Flux.update!(opt, ps, gs)
-
-            l += loss(x, y)
         end
-        @info "loss $f: $(l/100)"
+        @info "loss $t: $(l/100)"
+
+        if t%15 == 0
+            opt.eta /= 2
+        end
     end
 end
 
-testing_loader = data_fragments[end]
-test_loss = 0f0
-for (x, y) in testing_loader
-    x = is_gpu ? x |> gpu : x
-    y = is_gpu ? y |> gpu : y
-    test_loss += loss(x, y)
-end
-@info "Out data loss: $(test_loss/100)"
+# testing_loader = data_fragments[end]
+# test_loss = 0f0
+# for (x, y) in testing_loader
+#     x = is_gpu ? x |> gpu : x
+#     y = is_gpu ? y |> gpu : y
+#     test_loss += loss(x, y)
+# end
+# @info "Out data loss: $(test_loss/100)"
