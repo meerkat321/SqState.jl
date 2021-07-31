47dbfca6970d412aacad1819b2ea243d9286e9a4
JingYu Ning <foldfelis@gmail.com>
2021-07-31 17:54:51 +0800

    add heatmap and callback mechanism

--- src/real_time_system.jl
+++ src/real_time_system.jl
@@ -12,10 +12,26 @@ function banner()
     ])
 end
 
+function ctl(init_filename::String)
+    return html_div([
+        html_h2("Inference"),
+        html_div("Enter file name: "),
+        dcc_input(id="file-name", value=init_filename, type="text"),
+        html_button(id="submit-button-state", children="submit", n_clicks=0),
+    ])
+end
+
 color_config = [:colorscale=>"twilight", :cmid=>0, :zmid=>0]
 
 function get_surface(data::Vector, width::Integer, height::Integer)
     data = PlotlyJS.surface(z=data; color_config...)
+    layout = Layout(title="Wigner Function", width=width, height=height)
+
+    return plot(data, layout)
+end
+
+function get_heatmap(data::Vector, width::Integer, height::Integer)
+    data = PlotlyJS.heatmap(z=data; color_config...)
     layout = Layout(title="Wigner Function", width=width, height=height)
 
     return plot(data, layout)
@@ -28,24 +44,41 @@ function get_density_matrix(data::Vector, width::Integer, height::Integer)
     return plot(data, layout)
 end
 
-function plots(width::Integer, height::Integer)
-    ρ, w = infer("SQ20_5mW.mat")
+function get_plots(filename::String, width::Integer, height::Integer)
+    isempty(filename) && (return []) # TODO: handle non-exist file
+    ρ, w = infer(filename)
 
+    return [
+        dcc_graph(figure=get_surface(w, width, height)),
+        dcc_graph(figure=get_heatmap(w, width, height)),
+        dcc_graph(figure=get_density_matrix(ρ, width, height))
+    ]
+end
+
+function plots(init_filename::String, width::Integer, height::Integer)
     return html_div(
-        style=Dict("columnCount"=>2),
-        [
-            dcc_graph(id="surface-graph", figure=get_surface(w, width, height)),
-            dcc_graph(id="density-matrix-graph", figure=get_density_matrix(ρ, width, height))
-        ]
+        id="plots",
+        style=Dict("columnCount"=>3),
+        get_plots(init_filename, width, height)
     )
 end
 
-function gen_app()
+function gen_app(; width=500, height=500, init_filename="SQ20_5mW.mat")
     app = dash(external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
     app.layout = html_div([
         banner(),
-        plots(700, 700)
+        ctl(init_filename),
+        plots(init_filename, width, height),
     ])
+
+    callback!(
+        app,
+        Output("plots", "children"),
+        Input("submit-button-state", "n_clicks"),
+        State("file-name", "value")
+    ) do _, input_value
+        get_plots(input_value, width, height)
+    end
 
     return app
 end
