6763d3ad2c55320bdaee51f0be94c86bee944fbc
JingYu Ning <foldfelis@gmail.com>
2021-08-19 11:53:23 +0800

    try more fourier operator

--- src/model.jl
+++ src/model.jl
@@ -1,7 +1,7 @@
 export model
 
-function fourier_block(ch::Pair{S, S}, modes::NTuple{N, S}, pool_size::S, σ=gelu) where {N, S<:Integer}
-    pool = (pool_size > 0) ? MaxPool((pool_size, )) : identity
+function fourier_block(ch::Pair{S, S}, modes::NTuple{N, S}, pool_size::S, σ) where {N, S<:Integer}
+    pool = (pool_size > 0) ? MeanPool((pool_size, )) : identity
 
     return Chain(
         FourierOperator(ch, modes, σ, permuted=true),
@@ -12,27 +12,24 @@ end
 
 function model()
     modes = (64, )
+    σ = gelu
 
     return Chain(
         # stage 0
         BatchNorm(1),
+        Conv((3, ), 1=>64, pad=1, σ),
 
         # fourier operator
-        fourier_block(1=>8, modes, -1),
-        fourier_block(8=>16, modes, -1),
-        fourier_block(16=>32, modes, -1),
-        fourier_block(32=>64, modes, 2),
-        fourier_block(64=>128, modes, 2),
-        fourier_block(128=>64, modes, 2),
-        fourier_block(64=>32, modes, 2),
-        fourier_block(32=>16, modes, 2),
-        fourier_block(16=>8, modes, 2),
+        fourier_block(64=>64, modes, 0, σ),
+        fourier_block(64=>64, modes, 2, σ),
+        fourier_block(64=>32, modes, 2, σ),
+        fourier_block(32=>16, modes, 4, σ),
+        fourier_block(16=>8, modes, 4, σ),
 
         # stage 1
         flatten,
         Dense(8*64, 64, σ),
         Dense(64, 16, σ),
-        Dense(16, 8, σ),
-        Dense(8, 3, relu)
+        Dense(16, 3, relu)
     )
 end
