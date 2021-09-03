06c5f314ffdfce98d1a198fb091e8fa8d0106c1a
JingYu Ning <foldfelis@gmail.com>
2021-09-03 17:54:39 +0800

    update model

--- src/model.jl
+++ src/model.jl
@@ -1,7 +1,7 @@
 export model
 
 function fourier_block(ch::Pair{S, S}, modes::NTuple{N, S}, pool_size::S, σ) where {N, S<:Integer}
-    pool = (pool_size > 0) ? MeanPool((pool_size, )) : identity
+    pool = (pool_size > 0) ? MaxPool((pool_size, )) : identity
 
     return Chain(
         FourierOperator(ch, modes, σ, permuted=true),
@@ -17,19 +17,23 @@ function model()
     return Chain(
         # stage 0
         BatchNorm(1),
-        Conv((3, ), 1=>64, pad=1, σ),
+        Conv((1, ), 1=>64, σ),
+        BatchNorm(64),
 
         # fourier operator
         fourier_block(64=>64, modes, 0, σ),
+        fourier_block(64=>64, modes, 0, σ),
         fourier_block(64=>64, modes, 2, σ),
-        fourier_block(64=>32, modes, 2, σ),
-        fourier_block(32=>16, modes, 4, σ),
-        fourier_block(16=>8, modes, 4, σ),
+        fourier_block(64=>64, modes, 2, σ),
+        fourier_block(64=>64, modes, 4, σ),
+        fourier_block(64=>64, modes, 4, identity),
 
         # stage 1
+        Conv((1, ), 64=>8),
+        BatchNorm(8),
         flatten,
         Dense(8*64, 64, σ),
         Dense(64, 16, σ),
-        Dense(16, 3, relu)
+        Dense(16, 3)
     )
 end
