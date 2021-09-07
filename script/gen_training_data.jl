aa69d2b5534ab9ac033b7c69f09d7a2aa250ca6d
JingYu Ning <foldfelis@gmail.com>
2021-09-07 17:35:12 +0800

    update script

--- script/gen_training_data.jl
+++ script/gen_training_data.jl
@@ -1,6 +1,8 @@
 # using LinearAlgebra; LinearAlgebra.BLAS.set_num_threads(16)
 using Dates
 using SqState
+using JLD2
+using MAT
 
 @time for i in 1:101
     @show i
@@ -11,7 +13,7 @@ using SqState
         data_path = mkpath(SqState.training_data_path())
         jldsave(joinpath(data_path, "$file_name.jld2"); points, 𝛒s, args)
 
-        data_path = mkpath("~/training_data_mat")
+        data_path = mkpath("training_data_mat")
         file = matopen(joinpath(data_path, "$file_name.mat"), "w")
         write(file, "points", points); write(file, "dms", 𝛒s); write(file, "args", args)
         close(file)
