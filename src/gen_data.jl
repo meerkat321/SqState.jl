01f8df918bc5bb74bb02cbf28567c11c18974a88
meerkat321 <yrc13211@gmail.com>
2022-03-02 10:46:45 +0800

    Remove comment-out

--- src/gen_data.jl
+++ src/gen_data.jl
@@ -2,30 +2,30 @@ export
     gen_data_sqth_th,
     gen_data_sqth
 
-#function gen_data_sqth_th(;
-#    n_data, n_points=4096,
-#    r_range=(0., 2.), θ_range=(0., 2π), n̄_range=(0., 0.5),
-#    point_dim=1000, label_dim=100,
-#)
-#    args = Matrix{Float64}(undef, 6, n_data)
-#    points = Array{Float64, 3}(undef, 2, n_points, n_data)
-#    𝛒s = Array{ComplexF64, 3}(undef, label_dim, label_dim, n_data)
-#    σs = Matrix{Float64}(undef, n_points, n_data)
-#
-#    for i in 1:n_data
-#        args[:, i] .= r, θ, n̄, c1, c2, c3 = rand_arg_sqth_th(r_range, θ_range, n̄_range)
-#
-#        # points
-#        point_dim = (r > 1) ? point_dim : label_dim
-#        state = construct_state_sqth_th(r, θ, n̄, c1, c2, c3, point_dim)
-#        _, _, σs[:, i] = gaussian_state_sampler!(view(points, :, :, i), state, 0.)
-#
-#        # 𝛒s
-#        𝛒s[:, :, i] = (r > 1) ? state.𝛒[1:label_dim, 1:label_dim] : state.𝛒
-#    end
-#
-#    return points, 𝛒s, args, σs
-#end
+function gen_data_sqth_th(;
+    n_data, n_points=4096,
+    r_range=(0., 2.), θ_range=(0., 2π), n̄_range=(0., 0.5),
+    point_dim=1000, label_dim=100,
+)
+    args = Matrix{Float64}(undef, 6, n_data)
+    points = Array{Float64, 3}(undef, 2, n_points, n_data)
+    𝛒s = Array{ComplexF64, 3}(undef, label_dim, label_dim, n_data)
+    σs = Matrix{Float64}(undef, n_points, n_data)
+
+    for i in 1:n_data
+        args[:, i] .= r, θ, n̄, c1, c2, c3 = rand_arg_sqth_th(r_range, θ_range, n̄_range)
+
+        # points
+        point_dim = (r > 1) ? point_dim : label_dim
+        state = construct_state_sqth_th(r, θ, n̄, c1, c2, c3, point_dim)
+        _, _, σs[:, i] = gaussian_state_sampler!(view(points, :, :, i), state, 0.)
+
+        # 𝛒s
+        𝛒s[:, :, i] = (r > 1) ? state.𝛒[1:label_dim, 1:label_dim] : state.𝛒
+    end
+
+    return points, 𝛒s, args, σs
+end
 
 function gen_data_sqth(;
     n_data, n_points=4096,
--- test/gen_data.jl
+++ test/gen_data.jl
@@ -4,12 +4,12 @@ using QuantumStateBase
 @testset "gen_data" begin
     n = 2
 
-    #points, 𝛒s, args, σs = gen_data_sqth_th(n_data=n)
-    #@test all([
-    #    size(points) == (2, 4096, n),
-    #    size(𝛒s) == (100, 100, n),
-    #    size(args) == (6, n)
-    #])
+    points, 𝛒s, args, σs = gen_data_sqth_th(n_data=n)
+    @test all([
+        size(points) == (2, 4096, n),
+        size(𝛒s) == (100, 100, n),
+        size(args) == (6, n)
+    ])
 
     points, 𝛒s, args, σs = gen_data_sqth(n_data=n)
     @test all([
